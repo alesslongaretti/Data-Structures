09257ba9c72015b84251be44b891dec746d94377
alesslongaretti <alesslongaretti@gmail.com>
2020-09-17 18:46:59 -0400

    finished stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -26,3 +26,101 @@ return elements in Last In First Out order.
 #             return None
 #         else:
 #             return self.storage.pop()
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def add_to_head(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next_node(self.head)
+            self.head = new_node
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next_node(new_node)
+            self.tail = new_node
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        else:
+            ret_value = self.head.get_value()
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            return ret_value
+    
+    def remove_tail(self):
+        if self.tail is None:
+            return None
+        elif self.head == self.tail:
+            current_tail = self.tail.get_value()
+            self.tail = None
+            self.head = None
+            return current_tail
+        else:
+            current_node = self.head
+            while current_node.get_next_node() != self.tail:
+                current_node = current_node.get_next_node()
+            current_tail = self.tail
+            self.tail = current_node
+            return current_tail.value
+            
+    def contains(self,value):
+        cur_node = self.head
+        while cur_node is not None:
+            if cur_node.get_value() == value:
+                return True
+        return False
+
+    def get_max(self):
+        pass
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.size += 1
+        return self.storage.add_to_tail(value)
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
